add1e3b9259bc86dab3dc41e59c289b9ebaca492
harirajv <harirajv@gmail.com>
2020-04-18 12:10:35 +0530

    added users, axios=mock-adapter

---
+++ src/components/user.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import axios from 'axios';
+import Card from './card';
+import { withRouter } from 'react-router-dom';
+
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            isLoading: false
+        };
+    }
+
+    componentDidMount() {
+        axios.get('/users')
+            .then(res => {
+                this.setState({ data: [res.data.users], isLoading: false })
+            });
+
+        this.setState({ isLoading:true })
+    }
+
+    render() {
+        const users = this.state.data.map(user => <Card dish={user}/>);
+        return (
+            <div>
+                {this.state.data.length !== 0 && (<div>{users}</div>)}
+            </div>
+        );
+    }
+}
+
+export default withRouter(Users);
--- src/index.js
+++ src/index.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import {Router,Link,Route,Switch} from "react-router-dom"
+import MockAdapter from 'axios-mock-adapter';
+import axios from 'axios';
 import { createHashHistory } from "history"
 import AdminComponent from "./components/admin"
 import Dishes from "./components/dish"
 import DishDetails from "./components/dishinfo"
+import Users from './components/user';
+
 const history = createHashHistory({
   basname: "",
   hashType: "slash"
 });
+
+const mock = new MockAdapter(axios);
+
+mock.onGet('/users').reply(200, {
+  users: [{
+    dish: {
+      id: 1,
+      image: 'https://image.shutterstock.com/image-vector/profile-blank-icon-empty-photo-600w-535853269.jpg'
+    }
+  }]
+});
+
 ReactDOM.render(
   <Router history={history}>
     <Switch>
       <Route exact path="/admin"><AdminComponent/></Route>
       <Route exact path="/dishes"><Dishes/></Route>
       <Route exact path="/dishes/:dishId"><DishDetails/></Route>
+      <Route exact path="/users"><Users/></Route>
     </Switch>
 </Router>,
   document.getElementById('root')
