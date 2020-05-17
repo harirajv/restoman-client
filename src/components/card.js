e5ff6a5293c0e7f3fd40e935b7ca0a0661d93915
sesha <seshamadhav1998@gmail.com>
2020-05-17 20:15:19 +0530

    Pagination changes and activate dish changes

--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,8 @@
 import React from "react"
 
 import {getState} from "./new_order"
+import axios from "axios"
+import config from "../config"
 class Card extends React.Component
 {
     constructor(props)
@@ -25,14 +27,37 @@ class Card extends React.Component
     }
     handleActivateDish()
     {
-        if (window.confirm("Are you sure to change the activation")) {
-            console.log("You pressed OK!");
+        var message="";
+        if(this.props.dish.is_active==true)
+        {
+            message="Are you sure you want to deactivate the dish";
+        }
+        else if(this.props.dish.is_active==false)
+        {
+            message="Are you sure you want to activate the dish";
+        }
+        
+        if (window.confirm(message)) {
+            
+            (async ()=>{
+            
+                const response = await axios({
+                    method: 'PUT',
+                    url:config.endpoints.dishes+"/"+this.props.dish.id,
+                    data : {is_active: !this.props.dish.is_active},
+                    headers: {'Authorization':'Bearer '+config.authorization.auth_token}
+                })
+                
+                this.setState({data:response.data,loading:false})
+            })();
           } else {
             console.log("You pressed Cancel!");
           }
+
     }
     render()
     {
+        
         return(
             
                 <div className="card" >
@@ -59,7 +84,9 @@ class Card extends React.Component
                         {this.props.role=="admin" && this.props.option=="view" && 
                             (
                             <div>
-                                <button onClick={this.handleActivateDish}>Activate/Deactivate</button>
+                                {this.props.dish.is_active==true && (<button onClick={this.handleActivateDish}>Deactivate</button>)}
+                                {this.props.dish.is_active==false && (<button onClick={this.handleActivateDish}>Activate</button>)}
+                                
                                 <button onClick={this.handleEditDish}>Edit Dish</button>
                             </div>
                             )
--- src/components/dish.js
+++ src/components/dish.js
@@ -4,7 +4,7 @@ import Card from "./card"
 import {withRouter} from "react-router-dom"
 import config from "../config"
 import NavBar from "./navbar";
-
+import lodash from "lodash"
 // function getState(dishId,quantity)
 // {
 //     var existing_order=this.state.order;
@@ -20,9 +20,15 @@ class Dishes extends React.Component
     constructor(props)
     {
         super(props);
-        this.state={data:[],loading:false,order:{}};
+        this.state={data:[],loading:false,order:{}, current_page:1};
         this.handleSave=this.handleSave.bind(this);
         // getState=getState.bind(this);
+        this.handleNextPage = this.handleNextPage.bind(this);
+        this.handlePrevPage = this.handlePrevPage.bind(this);
+        this.handleChangePage = this.handleChangePage.bind(this);
+        this.pagechange = this.pagechange.bind(this)
+        this.delayedCallback = lodash.debounce(this.pagechange,1000);
+        
     }
     componentDidMount()
     {
@@ -31,16 +37,87 @@ class Dishes extends React.Component
             const response = await axios({
                 method: 'GET',
                 url:config.endpoints.dishes,
+                params : {'per_page':1},
                 headers: {'Authorization':'Bearer '+config.authorization.auth_token}
             })
             console.log(response.headers)
-            this.setState({data:response.data,loading:false})
+            this.setState({data:response.data,loading:false, current_page:response.headers['page'], total_pages:response.headers['total-pages']})
         })();
         this.setState({loading:true})
     }
     handleSave()
     {
         console.log(this.state)
+    }
+    handleChangePage(e)
+    {
+        e.persist();
+        this.setState({current_page:e.target.value});
+        this.delayedCallback(e);
+        
+        
+    }
+    pagechange(e)
+    {
+        var page= parseInt(e.target.value);
+        
+        if(!page || page < 1 || page > this.state.total_pages)
+        {
+
+            this.setState({current_page:page});
+        }
+        else
+        {
+
+            (async ()=>{
+        
+                const response = await axios({
+                    method: 'GET',
+                    url:config.endpoints.dishes,
+                    params : {'per_page':1, 'page':e.target.value},
+                    headers: {'Authorization':'Bearer '+config.authorization.auth_token}
+                })
+                
+                this.setState({data:response.data,loading:false, current_page:response.headers['page'], total_pages:response.headers['total-pages']})
+            })();
+        }
+            
+       
+    }
+    
+        
+  
+    handleNextPage()
+    {
+        var current_page=this.state.current_page;
+        (async ()=>{
+            
+            const response = await axios({
+                method: 'GET',
+                url:config.endpoints.dishes,
+                params : {'per_page':1, 'page':parseInt(current_page)+1},
+                headers: {'Authorization':'Bearer '+config.authorization.auth_token}
+            })
+            console.log(response.headers)
+            this.setState({data:response.data,loading:false, current_page:response.headers['page'], total_pages:response.headers['total-pages']})
+        })();
+        
+    }
+    handlePrevPage()
+    {
+        var current_page=this.state.current_page;
+        (async ()=>{
+            
+            const response = await axios({
+                method: 'GET',
+                url:config.endpoints.dishes,
+                params : {'per_page':1, 'page':parseInt(current_page)-1},
+                headers: {'Authorization':'Bearer '+config.authorization.auth_token}
+            })
+            console.log(response.headers)
+            this.setState({data:response.data,loading:false, current_page:response.headers['page'], total_pages:response.headers['total-pages']})
+        })();
+        
     }
     render()
     {   
@@ -54,11 +131,11 @@ class Dishes extends React.Component
                 <div class="row">
                     <div class="col-lg-12 text-right">
                         <span class="m-r-xs">Pages
-                            <input class="form-control" style={{width:"70px",display: "inline"}}  type="number" min="1"/> of Total Pages
+                            <input class="form-control" value={this.state.current_page} style={{width:"70px",display: "inline"}}  type="number" onChange={this.handleChangePage}/> of {this.state.total_pages}
                         </span>
                         <div class="btn-group">
-                            <button class="btn"><i class="fa fa-chevron-left"></i></button>
-                            <button class="btn"><i class="fa fa-chevron-right"></i></button>
+                            <button onClick={this.handlePrevPage} disabled={this.state.current_page==1} class="btn"><i class="fa fa-chevron-left"></i></button>
+                            <button onClick={this.handleNextPage} disabled={this.state.current_page==this.state.total_pages} class="btn"><i class="fa fa-chevron-right"></i></button>
                         </div>
                     </div>
                 </div>
